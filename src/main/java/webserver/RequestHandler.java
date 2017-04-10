b99bb944d36a34c93081e7bb9ba5c2ee3cc8711b
yongjunjung <yjjung@woowahan.com>
2017-04-10 21:06:37 +0900

    #6 로그인 후 쿠키저장까지 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,18 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.IOUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+
+import static util.HttpRequestUtils.parseQueryString;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -24,14 +29,102 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+
+            if (line == null) {
+                return ;
+            }
+
+            String path = getRequestPath(line);
+
+            Map<String, String> headerMap = new HashMap<String, String>();
+
+            while (!"".equals(line)) {
+                line = br.readLine();
+                String[] headers = line.split(": ");
+                if (headers.length == 2) {
+                    headerMap.put(headers[0], headers[1]);
+                }
+            }
+
+            if ("/user/create".equals(path)) {
+                log.debug("회원가입");
+                String bodyData = IOUtils.readData(br, Integer.parseInt(headerMap.get("Content-Length")) + 1);
+                Map<String, String> paramMap = parseQueryString(bodyData);
+                User user = new User(paramMap.get("userId"), paramMap.get("password"), paramMap.get("name"), paramMap.get("email"));
+                log.debug("user={}", user.toString());
+
+                DataBase.addUser(user);
+
+                DataOutputStream dos = new DataOutputStream(out);
+                response302Header(dos);
+
+            } else if ("/user/login".equals(path)) {
+                log.debug("회원로그인");
+                String bodyData = IOUtils.readData(br, Integer.parseInt(headerMap.get("Content-Length")) + 1);
+                Map<String, String> paramMap = parseQueryString(bodyData);
+                String userId = paramMap.get("userId");
+                String password = paramMap.get("password");
+
+                User user = DataBase.findUserById(userId);
+                if (user == null || !password.equals(user.getPassword())) {
+                    log.debug("로그인 실패");
+                    DataOutputStream dos = new DataOutputStream(out);
+                    responseLoginFail(dos);
+                } else {
+                    log.debug("로그인 성공");
+                    DataOutputStream dos = new DataOutputStream(out);
+                    response302LoginSuccessHeader(dos);
+                }
+
+            }else {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
+
+    private String getRequestPath(String line) {
+        String[] tokens = line.split(" ");
+        return tokens[1];
+    }
+
+    private void response302LoginSuccessHeader(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found\r\n");
+            dos.writeBytes("Location: /index.html\n \r\n");
+            dos.writeBytes("Set-Cookie : logined=true; Path=/ \n \r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void responseLoginFail(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found\r\n");
+            dos.writeBytes("Location: /user/login_failed.html\n \r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: /index.html\n \r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
